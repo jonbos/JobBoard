7487b80b517d4b0065dd2923c9347c97ac6055ee
Jon Bos <bos.jon@gmail.com>
2020-02-16 13:22:37 -0600

    refactor: adds AddServlet and generic add.jsp page that brings in generic jobForm.jsp or employerForm.jsp

--- src/main/java/controller/AddServlet.java
+++ src/main/java/controller/AddServlet.java
@@ -7,35 +7,76 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Employer;
+import model.Job;
+
 /**
  * Servlet implementation class AddServlet
  */
-@WebServlet("/AddServlet")
+@WebServlet("/addServlet")
 public class AddServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AddServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public AddServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		String type = request.getParameter("type");
+		String path = "/index.jsp";
+
+		if (type.equals("employer")) {
+			EmployerDAO empDAO = new EmployerDAO();
+			String empName = request.getParameter("empName");
+			String empLocation = request.getParameter("empLocation");
+			String empDescription = request.getParameter("empDescription");
+
+			Employer emp = new Employer();
+			emp.setName(empName);
+			emp.setDescription(empDescription);
+			emp.setLocation(empLocation);
+
+			empDAO.insert(emp);
+
+		} else if (type.equals("job")) {
+			JobDAO jobDAO = new JobDAO();
+			EmployerDAO empDAO = new EmployerDAO();
+
+			String jobTitle = request.getParameter("jobTitle");
+			String jobDescription = request.getParameter("jobDescription");
+			Integer empID = Integer.parseInt(request.getParameter("jobEmployer"));
+			Employer jobEmployer = empDAO.get(empID);
+
+			Job job = new Job();
+			job.setEmployer(jobEmployer);
+			job.setJobDescription(jobDescription);
+			job.setTitle(jobTitle);
+
+			jobDAO.insert(job);
+
+		}
+
+		request.getServletContext().getRequestDispatcher(path).forward(request, response);
+
 	}
 
 }
